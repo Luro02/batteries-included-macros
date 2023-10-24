c5794a07a763b318d9468bd2ab9cd5723f56299a
luro02 <24826124+Luro02@users.noreply.github.com>
2023-10-24 11:04:39 +0200

    rewrite installation script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 # the nozzle_diameter set in the extruder config of the
 # printer. The functionality will not be added to klipper
 # so a python extension will be used instead.
-# 
+#
 # Related information:
 # - https://github.com/Klipper3d/klipper/issues/797
 # - https://github.com/Klipper3d/klipper/pull/4123
@@ -19,71 +19,34 @@
 # Force script to exit if an error occurs
 set -e
 
-KLIPPER_PATH="${HOME}/klipper"
-SYSTEMDDIR="/etc/systemd/system"
-EXTENSION_NAME="batteries_included_macros.py"
-EXTENSION_PATH="src/${EXTENSION_NAME}"
-SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
-
-# verify that klipper is installed
-function check_klipper() {
-    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
-        echo "Klipper service found!"
-    else
-        echo "Klipper service not found, please install Klipper first"
-        exit -1
-    fi
-}
-
-# check if the extension is already installed
-function check_installation() {
-    local -i is_installed=0
-
-    if [ -e "${KLIPPER_PATH}/klippy/extras/${EXTENSION_NAME}" ]; then
-        is_installed=1
-    fi
-
-    echo ${is_installed}
-}
-
-# link extension to Klipper
-function link_extension() {
-    echo "Linking extension to Klipper..."
-    ln -sf "${SRCDIR}/${EXTENSION_PATH}" "${KLIPPER_PATH}/klippy/extras/${EXTENSION_NAME}"
-}
-
-# restart klipper
-function restart_klipper() {
-    echo "Restarting Klipper..."
-    sudo systemctl restart klipper
-}
-
-# ensure that the installation script is not ran as root
 function verify_ready() {
     if [ "$(id -u)" -eq 0 ]; then
         echo "This script must not run as root"
         exit -1
     fi
-
-    check_klipper
 }
 
-while getopts "k:" arg; do
-    case ${arg} in
-        k) KLIPPER_PATH=${OPTARG} ;;
-    esac
-done
-
 verify_ready
-is_installed=$(check_installation)
 
-# if it is not installed, create a link to the script and restart klipper
-if [ ${is_installed} -eq 0 ]; then
-    link_extension
+# check that klippy python env is present:
+KLIPPY_PYTHON_ENV_ACTIVATE="${HOME}/klippy-env/bin/activate"
+if [ ! -f "$KLIPPY_PYTHON_ENV_ACTIVATE" ]; then
+    echo "$KLIPPY_PYTHON_ENV_ACTIVATE does not exist."
+    exit -2
 fi
 
-# always restart klipper, to load the updated code
-restart_klipper
+# activate the python environment:
+source $KLIPPY_PYTHON_ENV_ACTIVATE
 
+if [[ $(pip list | grep -F pexpect) ]]; then
+    echo "found pexpect installation"
+else
+    echo "pexpect not installed, installing..."
+    pip install --require-virtualenv pexpect
+fi
 
-exit 0
+# execute installation script:
+python install.py
+
+# deactivate python enviroment
+deactivate

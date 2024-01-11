daefb702bf10b3a3a85b3475f6c031ac533bc269
luro02 <24826124+Luro02@users.noreply.github.com>
2024-01-11 07:51:13 +0100

    expose `_PARSE_MARLIN_PARAMS` in python

--- src/batteries_included_macros.py
+++ src/batteries_included_macros.py
@@ -5,14 +5,20 @@ if sys.version_info.major != 3:
     raise EnvironmentError("python 3 is required for execution of %s" % sys.argv[0])
 
 class BatteriesIncludedMacros:
+    is_debug: bool
+
     def __init__(self, config):
         self.printer = config.get_printer()
         self.config = config
         # make extra data available to gcode macros:
         self.printer.add_object("extra", self)
 
+        self.is_debug = config.getboolean("is_debug", default=False)
+
+
         self.gcode = self.printer.lookup_object('gcode')
         self.gcode.register_command('TEST_BATTERIES_INCLUDED', self.cmd_TEST_BATTERIES_INCLUDED, desc=self.cmd_TEST_BATTERIES_INCLUDED_help)
+        self.gcode.register_command('_PARSE_MARLIN_PARAMS', self.cmd_PARSE_MARLIN_PARAMS, desc=self.cmd_PARSE_MARLIN_PARAMS_help)
 
     def get_status(self, eventtime):
         bed_mesh_section = self.config.getsection('bed_mesh')
@@ -37,6 +43,43 @@ class BatteriesIncludedMacros:
         ]
 
         gcmd.respond_info(f"For reference here are some config values: {', '.join(values)}")
+
+    def _list_commands(self) -> list[str]:
+        # gcode_handlers is a dict[name, function]
+        # calling sorted on a dict, will only return the sorted keys
+        return list(sorted(self.gcode.gcode_handlers))
+
+    cmd_PARSE_MARLIN_PARAMS_help = ("Translates marlin params into klipper params")
+    def cmd_PARSE_MARLIN_PARAMS(self, gcmd):
+        target = gcmd.get('TARGET', default=None)
+        if target is None:
+            raise gcmd.error("TARGET must be specified")
+
+        if target not in self._list_commands():
+            raise gcmd.error(f"The command TARGET='{target}' is not known.")
+
+        args = str(gcmd.get('ARGS', default=""))
+
+        # This parses the args by first removing trailing comments with ; and '#', then splitting
+        # the result by whitespace (to obtain a list of arguments)
+        parsed_params = args.split(';', 1)[0].split('#', 1)[0].lower().split()
+
+        if self.is_debug:
+            gcmd.respond_info(f"ARGS: {args}, parsed: {parsed_params}")
+
+        macro_arguments = {}
+        for param in parsed_params:
+            if len(param) > 1:
+                macro_arguments[param[0:1]] = param[1:]
+            else:
+                # flags are set to true:
+                macro_arguments[param] = "true"
+
+        final_arguments = " ".join([f"{k}=\"{v}\"" for (k, v) in macro_arguments.items()])
+        if self.is_debug:
+            gcmd.respond_info(f"Calling: {target} {final_arguments}")
+
+        self.gcode.run_script_from_command(f"{target} {final_arguments}")
 
 def load_config(config):
     return BatteriesIncludedMacros(config)
